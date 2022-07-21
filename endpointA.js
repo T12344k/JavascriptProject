f37089722cbfd9e14a84dc6bcb7cecbbfb1b6672
T12344k <93878254+T12344k@users.noreply.github.com>
2022-07-21 22:35:23 +0200

    Update endpointA.js

--- endpointA.js
+++ endpointA.js
@@ -1,17 +1,21 @@
+//defines outcome as a boolean so it can be returned 
 const outcome = new Boolean(false);
 
-
+//function to split words and sort them alphabetically
 function handleStr(str){
     return str.toLowerCase().split("").sort().join("")
 }
 
+//function to check if two words are anagrams
 function anagram(str1, str2) {
+    //check if the two words have the same length
       if (str1.length !== str2.length) {
         return false;
       } else {
         if (
+            //if the two words, are equal if sorted alphabetically, they are angrams
             handleStr(str1) ===
-          handleSte(str2)
+          handleStr(str2)
         ) {
           return true;
         } 
@@ -19,6 +23,7 @@ function anagram(str1, str2) {
         
       }
     }
+//test with the following words
     
     console.log(anagram("hello", "olleh"));
     console.log(anagram("ronak", "konar"));
